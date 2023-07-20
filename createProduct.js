3f17d2e864aab7dd1b33c7469a6b4a6718105037
lucvo01 <luke.syvo@gmail.com>
2023-07-20 22:09:15 +0000

    .

--- createProduct.js
+++ createProduct.js
@@ -12,8 +12,10 @@ const createProduct = (numberOfProducts) => {
   for (let i = 0; i < numberOfProducts; i++) {
     const product = {
       name: faker.commerce.productName(),
-      price: faker.commerce.price({ min: 100, max: 200, dec: 0, symbol: "$" })
+      price: faker.commerce.price({ min: 100, max: 200, dec: 0, symbol: "$" }),
+      url: faker.image.urlLoremFlickr({ category: 'fashion' })
     };
+    console.log("Products", product);
   }
 };
 
