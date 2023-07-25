e453352a89cff1e389f0479a6536991786821587
Luc Vo <luke.syvo@gmail.com>
2023-07-25 09:25:53 -0600

    .

--- createProduct.js
+++ createProduct.js
@@ -16,7 +16,7 @@ const createProduct = (numberOfProducts) => {
     const product = {
       name: faker.commerce.productName(),
       description: faker.lorem.paragraph({ min: 1, max: 3 }),
-      price: faker.commerce.price({ min: 100, max: 200, dec: 0, symbol: "$" }),
+      price: faker.number.float({ min: 100, max: 200, precision: 0.01 }),
       image: faker.image.urlLoremFlickr({ category: "fashion" }),
       isDeleted: false
     };
