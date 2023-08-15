285cf5941c8fe13635786808b1897107937f69eb
Luc Vo <luke.syvo@gmail.com>
2023-08-15 11:15:04 -0600

    Update authController.loginWithEmail so user cant login when deactivated

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -13,7 +13,8 @@ authController.loginWithEmail = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email }, "+password");
-  if (!user) throw new AppError(400, "Invalid Credentials", "Login Error");
+  if (!user || user.isDeleted)
+    throw new AppError(400, "Invalid Credentials", "Login Error");
 
   const isMatch = await bcrypt.compare(password, user.password);
   if (!isMatch) throw new AppError(400, "Wrong password", "Login Error");
